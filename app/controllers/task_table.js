e340c3235c2490f08ce98b49ff590328af151c74
Qblack <blac2410@mylaurier.ca>
2015-06-26 16:16:25 -0400

    Added modal for edit

--- app/controllers/task_table.js
+++ app/controllers/task_table.js
@@ -3,16 +3,20 @@
  */
 'use strict';
 
-angular.module('taskminder.task_table',['ngRoute'])
-    .config(['$routeProvider', function($routeProvider){
+angular.module('taskminder.task_table',['ngRoute','ui.bootstrap']);
+angular.module('taskminder.task_table').config(['$routeProvider', function($routeProvider){
         $routeProvider.when('/tasks',{
             templateUrl: 'views/task_table.html',
             controller: 'TaskTableCtrl'
         });
-    }])
-    .controller("TaskTableCtrl",['$scope','Tasks','$cookies','Courses',function($scope,Tasks,$cookies,Courses){
+    }]);
+
+angular.module('taskminder.task_table').controller("TaskTableCtrl",
+    ['$scope','Tasks','$cookies','Courses','$modal','$log',
+        function($scope,Tasks,$cookies,Courses,$modal,$log){
         $scope.show_all = false;
         $scope.user_id = 1;// $cookies.get('user_id');
+        $scope.animationsEnabled = true;
 
         //The shows
         $scope.show = {
@@ -80,5 +84,124 @@ angular.module('taskminder.task_table',['ngRoute'])
             $scope.updateTask(task);
         };
 
-      
-    }]);
+        $scope.openEdit = function (task,size) {
+
+            var modalInstance = $modal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'edit_task.html',
+                controller: 'TaskModalInstanceCtrl',
+                size:size,
+                resolve: {
+                    task: function () {
+                        return task;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (task) {
+                $scope.selected = task; //TODO set to where it needs to be
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.toggleAnimation = function () {
+            $scope.animationsEnabled = !$scope.animationsEnabled;
+        };
+
+}]);
+
+angular.module('taskminder.task_table').controller('TaskModalInstanceCtrl',
+    function ($scope,  $modalInstance, task) {
+        console.log(task);
+
+
+        $scope.task = task;
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.task);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.today = function() {
+            $scope.task.due_date = new Date();
+        };
+
+        $scope.clear = function () {
+            $scope.task.due_date = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 2);
+        $scope.events =
+            [
+                {
+                    date: tomorrow,
+                    status: 'full'
+                },
+                {
+                    date: afterTomorrow,
+                    status: 'partially'
+                }
+            ];
+
+        $scope.getDayClass = function(date, mode) {
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+                for (var i=0;i<$scope.events.length;i++){
+                    var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+            return '';
+        };
+
+        $scope.task.due_time = new Date();
+        $scope.hstep = 1;
+        $scope.mstep = 15;
+
+        $scope.options = {
+            hstep: [1, 2, 3],
+            mstep: [1, 5, 10, 15, 25, 30]
+        };
+
+        $scope.ismeridian = true;
+        $scope.changed = function () {
+        };
+
+
+    });
